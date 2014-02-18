f8758f1040713eb67de118c1f13688d8a92ad066
Rick Wang <rickwangtw@hotmail.com>
2014-02-18 00:07:35 -0800

    Initialize Rails structure with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EvoPos::Application.config.secret_key_base = '616deba66e86d6825077c90bf2e35fae1d4dd356193b5871c801b726dba6d6011e17182db418ede86b1c37c5d104af640a5cf5a5228ce7af2701a83d9867df22'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EvoPos::Application.config.session_store :cookie_store, key: '_evo-pos_session'

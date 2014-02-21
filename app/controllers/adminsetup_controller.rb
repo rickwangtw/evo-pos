26f74e1c9d66b5b52416a5c71ca016e7d5f9c534
Rick Wang <rickwangtw@hotmail.com>
2014-02-21 09:49:24 -0800

    AdminSetup: redirect to login page if admin exists

--- app/controllers/adminsetup_controller.rb
+++ app/controllers/adminsetup_controller.rb
@@ -3,6 +3,7 @@ class AdminsetupController < ApplicationController
     has_admin = User.where(is_admin: true).first
     if has_admin != nil
       puts "Admin exists!"
+      redirect_to sessions_login_url
     end
     @user = User.new
   end

fafb18814282bb90e42866dedd7a9b5e5c81ecdf
Rick Wang <rickwangtw@hotmail.com>
2014-02-22 11:33:48 -0800

    Menu: add edit function

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -3,12 +3,26 @@ class MenuController < ApplicationController
     @menu = Menu.new
   end
   def create
-    @menu = Menu.new(params[:item].permit(:name, :description, :is_available))
+    @menu = Menu.new(params[:menu].permit(:name, :description, :is_available))
     if @menu.save
       redirect_to action: :index
     else
       render "new"
     end
+  end
+  def edit
+    @menu = Menu.find(params[:id])
+  end
+  def update
+    @menu = Menu.find(params[:id])
+    if @menu.update(params[:menu].permit(:name, :description, :is_available))
+      redirect_to action: :index
+    else
+      render 'edit'
+    end
+  end
+  def show
+    @menu = Menu.find(params[:id])
   end
   def index
     @menus = Menu.all

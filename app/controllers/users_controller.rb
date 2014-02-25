9cafe128afc436c0eec9ed3d9d0ca46586b57616
Rick Wang <rickwangtw@hotmail.com>
2014-02-25 07:04:15 -0800

    Implement User edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @is_authorized_user = User.find_by(id: cookies[:user_id]).is_admin
   end
   def create
     @user = User.new(user_params)
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
     end
   end
   def admin_create
-    @user = User.new(params.require(:user).permit(:login, :password, :password_confirmation, :email, :is_admin))
+    @user = User.new(user_params)
     @user.is_admin = true
     if @user.save
       redirect_to action: :index
@@ -19,11 +20,24 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+  def edit
+    @user = User.find(params[:id])
+    @is_authorized_user = User.find_by(id: cookies[:user_id]).is_admin
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params[:user].permit(:login, :password, :password_confirmation, :email, :is_admin))
+      redirect_to action: :index
+    else
+      render 'edit'
+    end
+  end
   def index
+    @is_authorized_user = User.find_by(id: cookies[:user_id]).is_admin
     @users = User.all
   end
   private
   def user_params
-    params.require(:user).permit(:login, :password, :password_confirmation, :email)
+    params.require(:user).permit(:login, :password, :password_confirmation, :email, :is_admin)
   end
 end
